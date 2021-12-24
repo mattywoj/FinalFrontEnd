a7714ba0a6c23fab32108d7ccd83c66e8a527dbe
Matt <matthew.wojslaw@student.fairfield.edu>
2021-12-23 21:04:17 -0500

    message

--- src/app/Services/residentService.ts
+++ src/app/Services/residentService.ts
@@ -2,6 +2,6 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class residentService{
-findAllResidents = () => fetch('http://localhost:8080/api/v1/residents')
+findAllResidents = () => fetch('http://finalproject-env.eba-uxmtnhqk.us-east-2.elasticbeanstalk.com/api/v1/residents')
 .then(response => response.json())
 }
